00332901a433be551b273960083c65223eacc996
Kjones6052 <letkylehelpyou@gmail.com>
2024-08-27 16:41:01 -0700

    Updated The Calculator App

--- The_Calculator_App.py
+++ The_Calculator_App.py
@@ -5,44 +5,45 @@
 # set up to prevent an error from showing in the console.
 
 def add_numbers(a, b):
-    result = (a + b)
-    print(result)
+    return (int(a) + int(b))
 
 def subtract_numbers(a, b):
-    result = (a - b)
-    print(result)
+    return (int(a) - int(b))
 
 def multiply_numbers(a, b):
-    result = (a * b)
-    print(result)
+    return (int(a) * int(b))
 
 def divide_numbers(a, b):
     try:
-        result = (a / b)
-        print(result)
+        return (int(a) / int(b))
     except ZeroDivisionError:
         print("You cannot divide by zero, please try again.")
     
-math = input("What type of calculation would you like to perform? You can choose from Addition, Subtraction, Multiplication, or Division: ")
-if math == "addition":
-    a = input("What is the first number you want to add?")
-    b = input("What is the number you want to add to the first number?")
-    result = int(a) + int(b)
-    print(result)
-elif math == "subtraction":
-    a = input("What is the first number you want to subtract?")
-    b = input("What is the number you want to subtract from the first number?")
-    result = int(a) - int(b)
-    print(result)
-elif math == "multiplication":
-    a = input("What is the first number you want to multiply?")
-    b = input("What is the number you want to multiply the first number by?")
-    result = int(a) * int(b)
-    print(result)
-elif math == "division":
-    a = input("What is the first number you want to divide?")
-    b = input("What is the number you want to divide the first number by?")
-    result = int(a) / int(b)
-    print(result)
-else:
-    print("I'm sorry, I don't understand. Please start from the beginning.")
+while True:
+    math = input("What type of calculation would you like to perform? You can choose from Addition, Subtraction, Multiplication, or Division: ").lower()
+    if math == "addition":
+        a = input("What is the first number you want to add?")
+        b = input("What is the number you want to add to the first number?")
+        result = add_numbers(a, b)
+        print(result)
+    elif math == "subtraction":
+        a = input("What is the first number you want to subtract?")
+        b = input("What is the number you want to subtract from the first number?")
+        result = subtract_numbers(a, b)
+        print(result)
+    elif math == "multiplication":
+        a = input("What is the first number you want to multiply?")
+        b = input("What is the number you want to multiply the first number by?")
+        result = multiply_numbers(a, b)
+        print(result)
+    elif math == "division":
+        a = input("What is the first number you want to divide?")
+        b = input("What is the number you want to divide the first number by?")
+        result = divide_numbers(a, b)
+        print(result)
+    else:
+        print("I'm sorry, I don't understand. Please start from the beginning.")
+    
+    continue_input = input("Would you like to perform another calulation? (yes/no): ").lower()
+    if continue_input != "yes":
+        break
